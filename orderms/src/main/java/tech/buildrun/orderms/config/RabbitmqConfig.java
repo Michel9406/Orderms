12afc60ee044d9d95d0a19c8effee486179dc8ea
Michel <michel9406@hotmail.com>
2025-05-29 18:14:01 -0300

    :ok_hand:versão melhorada do projeto

---
+++ orderms/src/main/java/tech/buildrun/orderms/config/RabbitmqConfig.java
@@ -0,0 +1,11 @@
+package tech.buildrun.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+    public static final String ORDER_CREATED_QUEUE = "orderms-order-created";
+
+
+
+}
